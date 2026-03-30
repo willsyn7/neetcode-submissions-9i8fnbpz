edbb3fcf3a38d928476eba0948a13b779a545354
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-03-29 20:05:58 -0700

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+         const res = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let l = i + 1;
+        let r = nums.length - 1;
+
+        while (l < r) {                              // ✅ strict less-than
+            const sum = nums[i] + nums[l] + nums[r];
+            if (sum > 0) r--;
+            else if (sum < 0) l++;
+            else {
+                res.push([nums[i], nums[l], nums[r]]);
+                l++;
+                r--;
+                while (l < r && nums[l] === nums[l - 1]) l++;  // ✅ bounds first
+            }
+        }
+    }
+    return res;
+}
+}
