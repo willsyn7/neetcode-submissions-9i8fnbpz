cd042582869fec7d88d0954f7528a8e18dd4b41a
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-03-30 13:13:59 -0700

    Add: eating-bananas - submission-15

---
+++ Data Structures & Algorithms/eating-bananas/submission-15.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 0 ; 
+        let r = Math.max(...piles);
+        let res = r ;
+
+        while(l <= r){
+            let m  = Math.floor((r + l )/ 2);
+            let time = 0;
+
+            for(let i = 0 ; i < piles.length;i++){
+                time += Math.ceil(piles[i] / m);
+            }
+                
+                if(time <= h){
+                    res = m ;
+                    r = m - 1;
+                }else{
+                    l = m +1;
+                }
+            }
+        
+        return res;
+    }
+}
