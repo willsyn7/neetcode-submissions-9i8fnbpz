dfae76bf671bb306c8a50de156c3fdec26109853
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-03-30 11:21:40 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+        let matches = {
+            "}" : "{",
+            "]" : "[",
+            ")" : "("
+        }
+        for(let br of s){
+            if(Object.values(matches).includes(br)){
+                stack.push(br);
+            }else{
+                if(stack.length === 0)return false;
+                let open  = stack.pop();
+                if(matches[br] !== open)return false;
+            }
+        }
+        return stack.length === 0 
+    }
+}
