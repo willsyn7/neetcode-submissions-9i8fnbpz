15036be62de0d2bea36f419a991656532f7753e6
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-04-01 14:21:29 -0700

    Add: meeting-schedule - submission-16

---
+++ Data Structures & Algorithms/meeting-schedule/submission-16.js
@@ -0,0 +1,32 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+    if(intervals.length === 0 )return true;
+        intervals.sort((a,b) => a.start - b.start)
+ 
+
+        let prevEnd = intervals[0].end
+        console.log(prevEnd)
+
+        for(let i = 1 ; i < intervals.length;i++){
+            if(prevEnd > intervals[i].start){
+                return false;
+            }
+            prevEnd = Math.max(prevEnd,intervals[i].end);
+        }
+        return true;
+    }
+}
