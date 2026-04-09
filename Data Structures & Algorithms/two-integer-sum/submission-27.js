d70f23af744ef93035896945dc7950f0f5ba1d10
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-04-09 10:35:34 -0700

    Add: two-integer-sum - submission-27

---
+++ Data Structures & Algorithms/two-integer-sum/submission-27.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = new Map();
+
+        for(let i = 0 ; i < nums.length;i++){
+            let diff = target - nums[i];
+
+            if(map.has(diff))return [i,map.get(diff)];
+            
+            map.set(nums[i], i );
+        }
+        return -1;
+    }
+
+}
