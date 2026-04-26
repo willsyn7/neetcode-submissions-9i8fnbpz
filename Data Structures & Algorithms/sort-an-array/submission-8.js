6c7f54090c08f31d661940c83421e08113bbf2e4
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-04-26 14:17:10 -0700

    Add: sort-an-array - submission-8

---
+++ Data Structures & Algorithms/sort-an-array/submission-8.js
@@ -0,0 +1,22 @@
+class Solution {
+    sortArray(nums) {
+        this.mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    mergeSort(arr, l, r) {
+        if (l >= r) return;
+
+        let m = Math.floor((l + r) / 2);
+        this.mergeSort(arr, l, m);
+        this.mergeSort(arr, m + 1, r);
+
+        // Inlined merge
+        let temp = [], i = l, j = m + 1;
+        while (i <= m && j <= r)
+            temp.push(arr[i] <= arr[j] ? arr[i++] : arr[j++]);
+        while (i <= m) temp.push(arr[i++]);
+        while (j <= r) temp.push(arr[j++]);
+        for (let i = l; i <= r; i++) arr[i] = temp[i - l];
+    }
+}
