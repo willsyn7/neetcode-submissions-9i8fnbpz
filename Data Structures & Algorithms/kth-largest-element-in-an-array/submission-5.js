0b863c657aec35f50e079fbbbedca1474539cffc
willsyn7 <143673630+willsyn7@users.noreply.github.com>
2026-05-13 13:25:49 -0700

    Add: kth-largest-element-in-an-array - submission-5

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        if (!nums.length)return ;
+    
+        let piviot = nums[Math.floor(Math.random() *nums.length)];
+        
+        let left = nums.filter((x) => x > piviot);
+        let mid = nums.filter((x) => x === piviot);
+        let right = nums.filter((x) => x < piviot);
+
+        let L = left.length , M  = mid.length;
+        
+        if(L >= k){
+            return this.findKthLargest(left, k)
+        }else if(k > M + L ){
+            return this.findKthLargest(right, k - L - M )
+        }else{
+            return mid[0]
+        }
+
+    }
+}
